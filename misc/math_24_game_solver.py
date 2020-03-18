beb460eca37a2f8eeadcf273f0173ca2f348aec9
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-18 14:11:00 -0500

    v20200318_6

---
+++ misc/math_24_game_solver.py
@@ -0,0 +1,31 @@
+/*
+math 24 game solution
+*/
+
+def solve24(nums):
+    for x, y, z in product(syms, repeat=3):
+        for a, b, c, d in permutations(nums):
+            if round(x(y(a,b),z(c,d)),5) == 24:
+                return f"({a} {op[y]} {b}) {op[x]} ({c} {op[z]} {d})"
+            elif round(x(a,y(b,z(c,d))),5) == 24:
+                return f"{a} {op[x]} ({b} {op[y]} ({c} {op[z]} {d}))"
+            elif round(x(y(z(c,d),b),a),5) == 24:
+                return f"(({c} {op[z]} {d}) {op[y]} {b}) {op[x]} {a}"
+            elif round(x(y(b,z(c,d)),a),5) == 24:
+                return f"({b} {op[y]} ({c} {op[z]} {d})) {op[x]} {a}"
+    return '--Not Found--'
+ 
+if __name__ == '__main__':
+    #nums = eval(input('Four integers in the range 1:9 inclusive, separated by commas: '))
+    for nums in [
+        [1,1,1,1],
+        [2,2,2,2],
+        [3,3,3,3],
+        [4,4,4,4],
+        [5,5,5,5],
+        [6,6,6,6],
+        [7,7,7,7],
+        [8,8,8,8],
+        [9,9,9,9],  
+            ]:
+        print(f"solve24({nums}) -> {solve24(nums)}")
