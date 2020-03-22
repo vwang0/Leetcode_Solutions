da4fee6b1d62cdd1f60fa8919a6a2b8fd693bfe4
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-22 16:08:33 -0500

    v20200322_15

--- easy/0190. Reverse Bits.py
+++ easy/0190. Reverse Bits.py
@@ -19,7 +19,7 @@ class Solution:
     # @return an integer
     def reverseBits(self, n):
         ans = 0
-        for i in xrange(32):
+        for i in range(32):
             ans = (ans << 1) + (n & 1)
             n >>= 1
         return ans        
