a6e2f2897d708998599d2108f28f1f9016a0101b
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-23 14:03:00 -0500

    v20200323_12

--- easy/0225. Implement Stack using Queues.py
+++ easy/0225. Implement Stack using Queues.py
@@ -21,14 +21,14 @@ You must use only standard operations of a queue -- which means only push to bac
 Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
 You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
 """
-
+from collections import deque
 class MyStack(object):
     
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        
+        self._queue = deque()
 
     def push(self, x):
         """
@@ -36,29 +36,31 @@ class MyStack(object):
         :type x: int
         :rtype: None
         """
-        
+        q = self._queue
+        q.append(x)
+        for _ in range(len(q)-1):
+            q.append(q.popleft())
 
     def pop(self):
         """
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
-        
+        return self._queue.popleft()
 
     def top(self):
         """
         Get the top element.
         :rtype: int
         """
-        
+        return self._queue[0]
 
     def empty(self):
         """
         Returns whether the stack is empty.
         :rtype: bool
         """
-        
-
+        return not len(self._queue)
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
