0a1451c041a3f4e662ded71f4463a67e8dd3560b
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-24 12:02:02 -0500

    v20200324_10

--- easy/0257. Binary Tree Paths.py
+++ easy/0257. Binary Tree Paths.py
@@ -1,11 +1,8 @@
 """
 257. Binary Tree Paths
 Given a binary tree, return all root-to-leaf paths.
-
 Note: A leaf is a node with no children.
-
 Example:
-
 Input:
 
    1
@@ -13,8 +10,21 @@ Input:
 2     3
  \
   5
-
 Output: ["1->2->5", "1->3"]
-
 Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root: 
+            return []
+        if not root.left or not root.right: 
+            return [str(root.val)]
+        return [str(root.val) + '->' + i for i in self.rootToLeafPaths(root.left)] +
+             [str(root.val) + '->' + i for i in self.rootToLeafPaths(root.right)]
