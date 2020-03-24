19ae85606b1ea9f68242594ca7e1d3d27ce9d096
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-23 20:37:51 -0500

    v20200323_19

---
+++ easy/0242. Valid Anagram.py
@@ -0,0 +1,30 @@
+"""
+242. Valid Anagram
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic1, dic2 = {}, {}
+        for item in s:
+            dic1[item] = dic1.get(item, 0) + 1
+        for item in t:
+            dic2[item] = dic2.get(item, 0) + 1
+        return dic1 == dic2
+
+class Solution:
+    def isAnagram(self, s, t):
+        return Counter(s) == Counter(t)
