c6d77dd45f2fa0f296fbb23695b380f77a1d656c
Vic <bwang@go.wustl.edu>
2020-03-25 01:45:39 -0500

    v20200325_3

--- easy/0292. Nim Game.py
+++ easy/0292. Nim Game.py
@@ -7,11 +7,11 @@ Both of you are very clever and have optimal strategies for the game. Write a fu
 Example:
 
 Input: 4
-Output: false 
+Output: false
 Explanation: If there are 4 stones in the heap, then you will never win the game;
-             No matter 1, 2, or 3 stones you remove, the last stone will always be 
+             No matter 1, 2, or 3 stones you remove, the last stone will always be
              removed by your friend.
 """
 class Solution:
     def canWinNim(self, n: int) -> bool:
-        
+        return n % 4 != 0
