9e5efaf9de248ef7e5fd30d9c3e77d5fef8c7b8d
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-25 17:52:01 -0500

    v20200325

---
+++ Shell/0194. Transpose File.sh
@@ -0,0 +1,28 @@
+# 194. Transpose File
+# Given a text file file.txt, transpose its content.
+# You may assume that each row has the same number of columns and each field is separated by the ' ' character.
+# Example:
+# If file.txt has the following content:
+# name age
+# alice 21
+# ryan 30
+# Output the following:
+# name alice ryan
+# age 21 30
+
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if (FNR == 1) {
+            t[i] = $i;
+        } else {
+            t[i] = t[i] " " $i
+        }
+    }
+}
+END {
+    for (i = 1; t[i] != ""; i++) {
+        print t[i]
+    }
+}
+' file.txt
