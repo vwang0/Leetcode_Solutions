ad4a0e7ae62de04f2403fd3d15d0c81988aae87a
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-25 02:54:35 -0500

    v20200325_7

--- easy/0293. Flip Game.py
+++ easy/0293. Flip Game.py
@@ -15,3 +15,10 @@ Output:
 ]
 Note: If there is no valid move, return an empty list [].
 """
+class Solution:
+    def generatePossibleNextMoves(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        return [s[:i-1]+"--"+s[i+1:] for i in range(1, len(s)) if s[i-1:i+1]=="++"]
