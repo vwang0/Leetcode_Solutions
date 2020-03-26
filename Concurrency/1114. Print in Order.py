81976ec8737fb68c299c382fc1a59c737d42e2f2
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-25 23:52:32 -0500

    v20200325

---
+++ Concurrency/1114. Print in Order.py
@@ -0,0 +1,30 @@
+import threading
+class Foo:
+    def __init__(self):
+        self.__cv = threading.Condition()
+        self.__has_first = False
+        self.__has_secon = False
+
+    def first(self, printFirst) 
+        # printFirst() outputs "first". Do not change or remove this line.
+        with self.__cv:
+            printFirst()
+            self.__has_first = True
+            self.__cv.notifyAll()
+
+    def second(self, printSecond) 
+        # printSecond() outputs "second". Do not change or remove this line.
+        with self.__cv:
+            while not self.__has_first:
+                self.__cv.wait()
+            printSecond()
+            self.__has_second = True
+            self.__cv.notifyAll()
+
+    def third(self, printThird) 
+        # printThird() outputs "third". Do not change or remove this line.
+        with self.__cv:
+            while not self.__has_second:
+                self.__cv.wait() 
+            printThird()
+            self.__cv.notifyAll()            
