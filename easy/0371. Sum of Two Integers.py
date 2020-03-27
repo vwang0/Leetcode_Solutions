98a73d00a1e9275f90de20238333c8e7267f7901
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-03-27 16:13:24 -0500

    v20200327

---
+++ easy/0371. Sum of Two Integers.py
@@ -0,0 +1,20 @@
+"""
+371. Sum of Two Integers
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+Example 1:
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+Input: a = -2, b = 3
+Output: 1
+"""
+class Solution:
+    def getSum(self, a: int, b: int):
+        list=[a,b]
+        return sum(list)      
+
+class Solution:
+    def getSum(self, a: int, b: int):
+        for _ in range(32):
+            a, b = a^b, (a&b)<<1
+        return a if a & 0x80000000 else a & 0xffffffff
