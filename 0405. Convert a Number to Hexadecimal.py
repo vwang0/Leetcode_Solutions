fe963f4b1cc8b96da918497c9e136e6df8823bcf
Vic <bwang@go.wustl.edu>
2020-03-28 03:20:15 -0500

    v20200328

--- 0405. Convert a Number to Hexadecimal.py
+++ 0405. Convert a Number to Hexadecimal.py
@@ -29,8 +29,11 @@ class Solution:
         mp = '0123456789abcdef'  # like a map
         ans = ''
         for i in range(8):
-            n = num & 15       # this means num & 1111b
-            c = mp[n]          # get the hex char
+            # this means num & 1111b
+            n = num & 15
+            # get the hex char
+            c = mp[n]
             ans = c + ans
             num = num >> 4
-        return ans.lstrip('0')  #strip leading zeroes
+        #strip leading zeroes
+        return ans.lstrip('0')
