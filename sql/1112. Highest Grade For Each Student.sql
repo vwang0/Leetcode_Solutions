a8233cac94a66136cdddfd85a62be67583b9ce22
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-04-06 11:39:57 -0500

    v20200406

--- sql/1112. Highest Grade For Each Student.sql
+++ sql/1112. Highest Grade For Each Student.sql
@@ -38,3 +38,13 @@ Result table:
 +------------+-----------+-------+
 */
 
+SELECT student_id, MIN(course_id) AS course_id, grade
+FROM Enrollments
+WHERE (student_id, grade) IN (
+    SELECT student_id,MAX(grade) AS grade
+    FROM Enrollments
+    GROUP BY student_id
+    )
+GROUP BY student_id
+ORDER BY student_id
+
