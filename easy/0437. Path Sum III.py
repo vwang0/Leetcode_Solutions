f3c454368dfce2a85b026699f8c5d16330ef625b
bwang@go.wustl.edu <bwang@go.wustl.edu>
2020-04-06 20:51:55 -0500

    v20200406

---
+++ easy/0437. Path Sum III.py
@@ -0,0 +1,61 @@
+/*
+437. Path Sum III
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        def dfs(root, prevSum, sum):
+            if not root:
+                return 0
+            count = 0
+            currSum = prevSum + root.val
+            if currSum - sum in rec:
+                count += rec[currSum - sum]
+            if currSum in rec:
+                rec[currSum] += 1
+            else:
+                rec[currSum] = 1
+            count += dfs(root.left, currSum, sum)
+            count += dfs(root.right, currSum, sum)
+            rec[currSum] -= 1
+            return count
+            
+        rec = {0:1}
+        return dfs(root, 0, sum)        
