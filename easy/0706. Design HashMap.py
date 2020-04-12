0850cdc2ca55ba6a087f19a2562243ef8fcc1720
Vic <bwang@go.wustl.edu>
2020-04-12 04:14:18 -0500

    v20200412

--- easy/0706. Design HashMap.py
+++ easy/0706. Design HashMap.py
@@ -11,14 +11,14 @@ remove(key) : Remove the mapping for the value key if this map contains the mapp
 Example:
 
 MyHashMap hashMap = new MyHashMap();
-hashMap.put(1, 1);          
-hashMap.put(2, 2);         
+hashMap.put(1, 1);
+hashMap.put(2, 2);
 hashMap.get(1);            // returns 1
 hashMap.get(3);            // returns -1 (not found)
 hashMap.put(2, 1);          // update the existing value
-hashMap.get(2);            // returns 1 
+hashMap.get(2);            // returns 1
 hashMap.remove(2);          // remove the mapping for 2
-hashMap.get(2);            // returns -1 (not found) 
+hashMap.get(2);            // returns -1 (not found)
 
 Note:
 
