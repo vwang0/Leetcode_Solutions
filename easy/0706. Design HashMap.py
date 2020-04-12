aa68c1e0f1d71edfdae19093052cb0b99a86d30a
Vic <bwang@go.wustl.edu>
2020-04-12 04:41:36 -0500

    v20200412

--- easy/0706. Design HashMap.py
+++ easy/0706. Design HashMap.py
@@ -26,3 +26,35 @@ All keys and values will be in the range of [0, 1000000].
 The number of operations will be in the range of [1, 10000].
 Please do not use the built-in HashMap library.
 """
+class MyHashMap:
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
