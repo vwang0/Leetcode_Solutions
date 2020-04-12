dc5483c1560f1074267173994cdd496a0746bcce
vwang0 <bwang@go.wustl.edu>
2020-04-12 17:56:31 -0500

    v20200402

--- Algorithms_easy/0680. Valid Palindrome II.py
+++ Algorithms_easy/0680. Valid Palindrome II.py
@@ -12,3 +12,9 @@ Explanation: You could delete the character 'c'.
 Note:
 The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
 """
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        while i < len(s) / 2 and s[i] == s[-(i + 1)]: i += 1
+        s = s[i:len(s) - i]
+        return s[1:] == s[1:][::-1] or s[:-1] == s[:-1][::-1]        
