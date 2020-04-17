1246605962012c636dff4d18756ffa0d0f6e6d77
vwang0 <bwang@go.wustl.edu>
2020-04-17 03:08:22 -0500

    v20200417

---
+++ misc/reverseList.py
@@ -0,0 +1,13 @@
+"""
+reverse linked list
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+
