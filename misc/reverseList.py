df64a7ce66628c72be9e95967028fe56cf260bae
vwang0 <bwang@go.wustl.edu>
2020-04-17 03:10:53 -0500

    v20200417

--- misc/reverseList.py
+++ misc/reverseList.py
@@ -8,6 +8,13 @@ reverse linked list
 #         self.next = None
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
+    def reverseList(self, head: ListNode):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
 
 
