605e51acf0bdb367bb0b808147ca5e5f4ab3690b
vwang0 <bwang@go.wustl.edu>
2020-04-18 01:58:36 -0500

    v20200417

--- SQL/0597. Friend Requests I Overall Acceptance Rate.sql
+++ SQL/0597. Friend Requests I Overall Acceptance Rate.sql
@@ -28,14 +28,58 @@ the following result.
 |-----------|
 |       0.80|
 */
-WITH cte1 AS (
-    SELECT DISTINCT sender_id, send_to_id
-    FROM friend_request
-    )
-WITH cte2 AS (
-    SELECT DISTINCT requester_id,accepter_id
-    FROM request_accepted
-    )
+-- Q1:  Write a query to find the overall acceptance rate of requests 
+-- rounded to 2 decimals, which is the number of acceptance 
+-- divide the number of requests.
+-- Solution 1
+SELECT IFNULL(
+    ROUND(
+        (COUNT(DISTINCT requester_id, accepter_id))
+        /
+        (COUNT(DISTINCT sender_id, send_to_id))
+    ,2)
+,0.00) AS accept_rate
+FROM friend_request, request_accepted;
 
-SELECT ROUND(IFNULL((SELECT COUNT(*) FROM ct2)/(SELECT COUNT(*) FROM ct1),0),2)
-AS accept_rate
+-- Solution 2:
+SELECT ROUND(IFNULL(fr.acp/ra.sed, 0), 2) AS accept_rate
+FROM
+    (SELECT COUNT(DISTINCT requester_id, accepter_id) AS acp
+     FROM request_accepted) AS fr,
+
+    (SELECT COUNT(DISTINCT sender_id, send_to_id) AS sed
+     FROM friend_request) AS ra;
+
+-- Q2: create a subquery which contains count, month, 
+-- join each other with month and group by month so we can get the finally result.
+SELECT ROUND(IFNULL(fr.acp/ra.sed, 0), 2) AS accept_rate, fr.mon AS month
+FROM
+    (SELECT COUNT(DISTINCT requester_id, accepter_id) AS acp, MONTH(accept_date) AS mon
+     FROM request_accepted) AS fr,
+
+    (SELECT COUNT(DISTINCT sender_id, send_to_id) AS sed, MONTH(request_date) AS mon
+     FROM friend_request) AS ra
+WHERE fr.mon = ra.mon
+;
+
+-- Q3: Can you write a query to return the accept rate but for every month?
+
+SELECT s.date,
+       IFNULL(round(sum(IF(t.ind = 'a', t.cnt, 0))/sum(IF(t.ind = 'r', t.cnt, 0)),2),0) AS accept_rate
+FROM 
+    (SELECT distinct x.request_date AS date
+     FROM friend_request AS x
+     union SELECT distinct y.accept_date AS date
+     FROM request_accepted AS y) AS s
+LEFT JOIN
+    (SELECT v.request_date AS date,
+            count(*) AS cnt,
+            'r' AS ind
+     FROM friend_request AS v
+     GROUP BY v.request_date
+     union all select w.accept_date AS date,
+                      count(*) AS cnt,
+                      'a' AS ind
+     FROM request_accepted AS w
+     GROUP BY w.accept_date) AS t ON s.date >= t.date
+GROUP BY s.date
