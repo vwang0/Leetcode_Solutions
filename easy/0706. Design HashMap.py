9e337ccdf831f8e7ccb9894e2ac50f6968d02826
Vic <bwang@go.wustl.edu>
2020-04-24 04:04:36 -0500

    v20200424

--- easy/0706. Design HashMap.py
+++ easy/0706. Design HashMap.py
@@ -32,26 +32,56 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        
+        self.block = 10000
+        self._map = [None] * self.block
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        
+        slot = key % self.block
+        curr = self._map[slot]
+        while curr:
+            if curr.key == key:
+                curr.val = value
+                return
+            curr = curr.next
+        node = Node(key,value)
+        node.next = self._map[slot]
+        self._map[slot] = node
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
+        slot = key % self.block
+        curr = self._map[slot]
+        while curr:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
         
+        return -1
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
+        slot = key % self.block
+        curr = self._map[slot]
+        while curr:
+            if curr.key == key:
+                curr.key = -1
+                return
+            curr = curr.next
         
+        return
 
+class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
