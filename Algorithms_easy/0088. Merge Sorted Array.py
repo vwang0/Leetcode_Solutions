9e7536304a22d23279ec528783333d332d94894f
vwang0 <bwang@go.wustl.edu>
2020-04-26 11:39:34 -0500

    v20200426

--- Algorithms_easy/0088. Merge Sorted Array.py
+++ Algorithms_easy/0088. Merge Sorted Array.py
@@ -14,6 +14,7 @@ nums2 = [2,5,6],       n = 3
 
 Output: [1,2,2,3,5,6]
 """
+# Solution 1:
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         while n > 0:
@@ -25,8 +26,29 @@ class Solution(object):
                 m -= 1
         return nums1 
 
+# Solution 2: 
 class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        nums1[m:] = nums2[:n]
-        nums1.sort()        
-        return nums1 
+def merge(self, nums1, m, nums2, n):
+    l1, l2, end = m-1, n-1, m+n-1
+    while l1 >= 0 and l2 >= 0:
+        if nums2[l2] > nums1[l1]:
+            nums1[end] = nums2[l2]
+            l2 -= 1
+        else:
+            nums1[end] = nums1[l1]
+            l1 -= 1
+        end -= 1
+    if l1 < 0: # if nums2 left
+        nums1[:l2+1] = nums2[:l2+1]
+
+ def merge1(self, nums1, m, nums2, n):
+    m, n = m-1, n-1
+    while m >= 0 and n >= 0:
+        if nums1[m] > nums2[n]:
+            nums1[m+n+1] = nums1[m]
+            m -= 1
+        else:
+            nums1[m+n+1] = nums2[n]
+            n -= 1
+    if n != -1: # nums2 is still left
+        nums1[:n+1] = nums2[:n+1]
