a72f9aa11f0f68a9cb0c4bcd41931f21e8bf86ed
vwang0 <bwang@go.wustl.edu>
2020-05-21 15:19:05 -0500

    v20200521

--- Algorithms_easy/0443. String Compression.py
+++ Algorithms_easy/0443. String Compression.py
@@ -45,3 +45,21 @@ class Solution:
             chars[k] = i
             k += 1
         return k
+
+class Solution:
+    	def compress(self, chars: List[str]) -> int:
+		n = len(chars)
+		i,count = 0, 1
+		for j in range(1,n+1):
+			if j<n and chars[j]==chars[j-1]:
+				count += 1
+			else:
+				chars[i] = chars[j-1]
+				i+=1
+				if count>1:
+					for k in str(count):
+						chars[i] = k
+						i+=1
+				count = 1
+		chars = chars[:i]
+		return i
--- SQL/0550. Game Play Analysis IV.sql
+++ SQL/0550. Game Play Analysis IV.sql
@@ -41,8 +41,9 @@ SELECT ROUND(IFNULL(COUNT(t2.player_id)/COUNT(t1.player_id),0),2) AS fraction
 FROM
     (SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
-    GROUP BY player_id) t1 LEFT JOIN Activity t2
-    ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
+    GROUP BY player_id) t1 
+LEFT JOIN Activity t2
+ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
 ;
 
 SELECT ROUND(SUM(CASE
