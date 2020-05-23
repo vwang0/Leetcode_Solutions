11959396b48a6622e342cfc636c46187f6691fef
vwang0 <bwang@go.wustl.edu>
2020-05-22 19:43:37 -0500

    v20200522

---
+++ Algorithms_easy/0146. LRU Cache.py
@@ -0,0 +1,56 @@
+'''
+0146. LRU Cache
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+'''
+class ListNode(object):    
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = collections.OrderedDict()
+
+    def get(self, key: int):
+        if key not in self.dic:
+            return -1
+        val = self.dic[key]
+        self.dic.move_to_end(key)
+        return val        
+
+    def put(self, key: int, value: int):
+        self.dic[key] = value
+        self.dic.move_to_end(key)
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(last=False)        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
