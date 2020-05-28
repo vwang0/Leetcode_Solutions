bfbcd419af67c47a05c897739b95d9fb56b7c832
vwang0 <bwang@go.wustl.edu>
2020-05-28 00:01:39 -0500

    v20200528

--- Algorithms_hard/0124. Binary Tree Maximum Path Sum.py
+++ Algorithms_hard/0124. Binary Tree Maximum Path Sum.py
@@ -2,20 +2,34 @@
 0124. Binary Tree Maximum Path Sum
 
 """
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int maxPathSum(TreeNode* root) {
-        
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: TreeNode):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(root):
+            if not root:
+                return 0
+            l_max = dfs(root.left)
+            r_max = dfs(root.right)
+            if l_max < 0:
+                l_max = root.val
+            else:
+                l_max += root.val
+            if r_max < 0:
+                r_max = root.val
+            else:
+                r_max += root.val
+            self.maximum = max(self.maximum, l_max+r_max-root.val)
+            return max(l_max, r_max)
+           
+        self.maximum = -float('inf')
+        dfs(root)
+        return self.maximum         
