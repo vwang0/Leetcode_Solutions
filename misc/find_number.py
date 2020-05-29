9553f558fdd9982edd90cbbe46d723d0a8f6f790
vwang0 <bwang@go.wustl.edu>
2020-05-29 03:15:36 -0500

    v20200529

--- misc/find_number.py
+++ misc/find_number.py
@@ -1,16 +1,21 @@
-/*
+"""
 Find the smallest number N in a given range num that meet the criteria:
 N, 2N, 3N, 4N, and 5N have the same digits.
-*/
+"""
+
 from collections import Counter
 def findNum(N):
     for i in range(1,N):
-        if Counter(str(i))== Counter(str(2*i)) and Counter(str(2*i)) == Counter(str(3*i)) and Counter(str(3*i)) == Counter(str(4*i)) and Counter(str(4*i)) == Counter(str(5*i)):
+        if Counter(str(i))== Counter(str(2*i)) 
+        and Counter(str(2*i)) == Counter(str(3*i)) 
+        and Counter(str(3*i)) == Counter(str(4*i)) 
+        and Counter(str(4*i)) == Counter(str(5*i))
+        and Counter(str(5*i)) == Counter(str(6*i)) :
             return i
     return print('Not Found!')
 
 
-/*
+"""
 N = 1,000,000
 results：
 142857
@@ -30,4 +35,4 @@ results：
 14285700
 14298570
 14299857
-*/
+"""
