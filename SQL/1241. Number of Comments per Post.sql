ea8a1e195c363f5a1d4bc5184f92bb6b7e19e6bc
vwang0 <bwang@go.wustl.edu>
2020-06-03 18:57:07 -0500

    v20200603

--- SQL/1241. Number of Comments per Post.sql
+++ SQL/1241. Number of Comments per Post.sql
@@ -63,3 +63,21 @@ FROM   submissions a
 WHERE  a.parent_id IS NULL 
 GROUP  BY a.sub_id 
 ORDER  BY NULL 
+;
+
+
+SELECT s.sub_id AS post_id,
+       (SELECT COUNT(DISTINCT(s1.sub_id))
+        FROM Submissions s1
+        WHERE s1.parent_id = s.sub_id) AS number_of_comments
+FROM Submissions s
+WHERE s.parent_id IS null
+GROUP BY s.sub_id 
+;
+
+SELECT S1.sub_id AS post_id,
+       COUNT(DISTINCT S2.sub_id) AS number_of_comments
+FROM Submissions S1
+LEFT JOIN Submissions S2 ON S1.sub_id = S2.parent_id
+WHERE S1.parent_id IS NULL
+GROUP BY S1.sub_id
