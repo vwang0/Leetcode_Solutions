6118820b137d118e93cfd41cff2180bd400f2675
vwang0 <bwang@go.wustl.edu>
2020-06-14 22:58:24 -0500

    v20200614

--- Algorithms_medium/0368. Largest Divisible Subset.py
+++ Algorithms_medium/0368. Largest Divisible Subset.py
@@ -32,3 +32,27 @@ class Solution(object):
         return list(max(subsets.values(), key=len))
 
 
+class Solution(object):
+        def largestDivisibleSubset(self, nums):
+            """
+            :type nums: List[int]
+            :rtype: List[int]
+            """
+            if not nums:
+                return []
+            nums=sorted(nums)
+            cont=[1]*len(nums)
+            par=[-1]*len(nums)
+            m,mi=0,-1
+            for i in xrange(len(nums)):
+                for j in xrange(i):
+                    if not nums[i]%nums[j] and cont[i]<=cont[j]:
+                        cont[i]=cont[j]+1
+                        par[i]=j
+                if cont[i]>m:
+                    m,mi=cont[i],i
+            ans=[]
+            while mi>=0:
+                ans.append(nums[mi])
+                mi=par[mi]
+            return ans
