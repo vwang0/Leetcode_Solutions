b9de9b1b61ff8ae751655439a6d6f2db4634a18a
vwang0 <bwang@go.wustl.edu>
2020-06-27 17:23:17 -0500

    v20200627

--- Concurrency/1279. Traffic Light Controlled Intersection.py
+++ Concurrency/1279. Traffic Light Controlled Intersection.py
@@ -50,15 +50,22 @@ arrivalTimes is non-decreasing
 """
 import threading
 class TrafficLight:
-    def __init__(self):
-        self.__l = threading.Lock()
+    def __init__(self):        
         self.__light = 1
 
-    def carArrived(self, carId, roadId, direction, turnGreen, crossCar):
-        with self.__l:
+    def carArrived(
+        self,
+        carId: int,                      # ID of the car
+        roadId: int,                     # ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        direction: int,                  # Direction of the car
+        turnGreen: 'Callable[[], None]', # Use turnGreen() to turn light to green on current road
+        crossCar: 'Callable[[], None]'   # Use crossCar() to make car cross the intersection
+    ) :
+        
             if self.__light != roadId:
                 self.__light = roadId
                 turnGreen()
             crossCar()
+        
 
 
