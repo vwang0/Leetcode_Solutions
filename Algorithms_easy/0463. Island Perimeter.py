90157dce5ce722ad152a81ae79c0a7dbe8995ccc
vwang0 <bwang@go.wustl.edu>
2020-07-03 17:53:16 -0500

    20200703

---
+++ Algorithms_easy/0463. Island Perimeter.py
@@ -0,0 +1,39 @@
+"""
+0463. Island Perimeter
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example:
+
+Input:
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Output: 16
+
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+
+"""
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]):
+        res = 0
+        for row in range(len(grid)):
+            for column in range(len(grid[0])):
+                if grid[row][colmn]: 
+                    res += 4
+                    if row and grid[row-1][column]:
+                        res -= 2
+                    if column and grid[row][column-1]:
+                        res -= 2 
+        return res
+
+
+
+
