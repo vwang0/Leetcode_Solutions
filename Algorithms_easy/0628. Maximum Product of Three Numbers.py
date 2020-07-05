98ab64fbd4c5674e72d216c8ff62deaa6f7554be
vwang0 <bwang@go.wustl.edu>
2020-07-05 02:09:07 -0500

    v20200705

--- Algorithms_easy/0628. Maximum Product of Three Numbers.py
+++ Algorithms_easy/0628. Maximum Product of Three Numbers.py
@@ -22,4 +22,4 @@ Multiplication of any three numbers in the input won't exceed the range of 32-bi
 class Solution:
     def maximumProduct(self, nums: List[int]):
         nums = sorted(nums)
-        return max(nums[-1]*nums[0]*nums[1],max(nums[-1]*nums[-2]*nums[-3],nums[-1]*nums[-2]*nums[0],))
+        return max(nums[-1]*nums[0]*nums[1],max(nums[-1]*nums[-2]*nums[-3],nums[-1]*nums[-2]*nums[0]))
