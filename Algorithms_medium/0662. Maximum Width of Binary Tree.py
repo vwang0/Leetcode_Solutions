80f8dd621559e93a30871e28574e1c90bc37b102
vwang0 <bwang@go.wustl.edu>
2020-07-09 09:48:48 -0500

    20200709

---
+++ Algorithms_medium/0662. Maximum Width of Binary Tree.py
@@ -0,0 +1,82 @@
+"""
+0662. Maximum Width of Binary Tree
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode):
+        if not root: return 0
+        q = [(root,1)] 
+        res = 1
+        lastNode = root
+        while len(q) > 0:
+            node, idx = q.pop(0)
+            if node.left:
+                q.append((node.left, idx*2))
+            if node.right:
+                q.append((node.right, idx*2+1))
+            if node == lastNode:
+                if len(q) > 0:
+                    res = max(res, q[-1][1]-q[0][1]+1)
+                    lastNode = q[-1][0]
+        return res
+
+
+
