1cb2885ad781f09c309cb42d065a31f4235ac022
vwang0 <bwang@go.wustl.edu>
2020-07-11 21:13:16 -0500

    20200711

--- Algorithms_easy/0246. Strobogrammatic Number.py
+++ Algorithms_easy/0246. Strobogrammatic Number.py
@@ -16,7 +16,7 @@ class Solution(object):
     def isStrobogrammatic(self, num):
         table = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         n = len(num)
-        for i in range((n >> 1) + 1):
+        for i in range((n//2) + 1):
             if num[i] not in table.keys() or table[num[i]] != num[n - i - 1]:
                 return False
         return True
@@ -30,12 +30,15 @@ class Solution(object):
         """
         len2strs = { "00", "11", "88", "69", "96" }
         len1strs = { "0", "1", "8" }
-
         s = str(num)
-
         while len(s)>2:
             if s[0]+s[-1] not in len2strs:
                 return False
             s = s[1:-1]
-
         return s in len2strs or s in len1strs
+
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        rotates = {"0": "0", "1": "1", "8": "8", "6": "9", "9": "6"}
+        return all(b in rotates and rotates[b] == a
+                   for a, b in zip(num, num[::-1]))
