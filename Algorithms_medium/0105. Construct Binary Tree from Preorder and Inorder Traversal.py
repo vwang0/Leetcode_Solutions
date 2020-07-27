e8dd07855421021cf4ab50183c56dd60c354260d
vwang0 <bwang@go.wustl.edu>
2020-07-27 06:17:27 -0500

    20200727

--- Algorithms_medium/0105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Algorithms_medium/0105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -28,11 +28,14 @@ class Solution:
         def helper(pre_left: int, pre_right: int, in_left: int, in_right: int):
             if pre_left > pre_right: return None
             pre_root = pre_left
-            in_root = index[preorder[pre_root]]
+            in_root = idx_map[preorder[pre_root]]
             root = TreeNode(preorder[pre_root])
             size_left = in_root - in_left
-            root.left = helper(pre_left + 1, pre_left + size_left, in_left, in_root - 1)
-            root.right = helper(pre_left + size_left + 1, pre_right, in_root + 1, in_right)
+            root.left = helper(pre_left + 1, pre_left + size_left, in_left,
+                               in_root - 1)
+            root.right = helper(pre_left + size_left + 1, pre_right,
+                                in_root + 1, in_right)
             return root
-        index = {element: i for i, element in enumerate(inorder)}
-        return helper(0, n - 1, 0, n - 1)
+
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        return helper(0, len(preorder) - 1, 0, len(preorder) - 1)
