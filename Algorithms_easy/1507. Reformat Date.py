ac904edf42e52570aae911302c66a2f3dd57ff07
vwang0 <bwang@go.wustl.edu>
2020-07-31 13:40:27 -0500

    20200731

--- Algorithms_easy/1507. Reformat Date.py
+++ Algorithms_easy/1507. Reformat Date.py
@@ -30,10 +30,25 @@ Constraints:
 
 The given dates are guaranteed to be valid, so no error handling is necessary.
 """
+
+
 class Solution:
     def reformatDate(self, date: str) -> str:
-    di={"Jan":'01', "Feb":'02', "Mar":'03', "Apr":'04', "May":'05', "Jun":'06', "Jul":'07', "Aug":'08', "Sep":'09', "Oct":'10', "Nov":'11', "Dec":'12'}
-    s=date.split()
-    if len(s[0]) <4:
-        return s[2]+"-"+di[s[1]]+"-0"+s[0][:-2]
-    return s[2]+"-"+di[s[1]]+"-"+s[0][:-2]
+        dic = {
+            "Jan": '01',
+            "Feb": '02',
+            "Mar": '03',
+            "Apr": '04',
+            "May": '05',
+            "Jun": '06',
+            "Jul": '07',
+            "Aug": '08',
+            "Sep": '09',
+            "Oct": '10',
+            "Nov": '11',
+            "Dec": '12'
+        }
+        s = date.split()
+        if len(s[0]) < 4:
+            return s[2] + "-" + dic[s[1]] + "-0" + s[0][0]
+        return s[2] + "-" + dic[s[1]] + "-" + s[0][0:2]
