165233334c70cff95b3a5216a9a988912bbf83d8
vwang0 <bwang@go.wustl.edu>
2020-08-01 03:36:00 -0500

    20200801

---
+++ Algorithms_easy/0520. Detect Capital.py
@@ -0,0 +1,40 @@
+"""
+0520. Detect Capital
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+ 
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+"""
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) :
+        if word.upper() == word or word.lower() == word or word.capitalize(
+        ) == word:
+            return True
+        return False
+
+
+
+class Solution:
+    def detectCapitalUse(self, word: str):
+        return re.fullmatch(r'[A-Z]*|.[a-z]*', word)
