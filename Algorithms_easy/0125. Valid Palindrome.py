72f9723c216dd895483befcae76d1cc091a41cfc
vwang0 <bwang@go.wustl.edu>
2020-08-03 08:45:32 -0500

    20200803

--- Algorithms_easy/0125. Valid Palindrome.py
+++ Algorithms_easy/0125. Valid Palindrome.py
@@ -16,15 +16,14 @@ Output: false
 class Solution(object):
     def isPalindrome(self, s):
         if not s: return True
-        i, j = 0, len(s) - 1
-        while i < j:
-            while i < j and not s[i].isalnum(): 
-                i += 1
-            while i < j and not s[j].isalnum(): 
-                j -= 1
-            if s[i].lower() != s[j].lower():
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
-            i += 1; j -= 1
+            left += 1
+            right -= 1
         return True
-
-        
