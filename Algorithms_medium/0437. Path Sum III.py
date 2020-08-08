09c5f29f91e7e34444cca9cc21248dccb2e4f1d2
vwang0 <bwang@go.wustl.edu>
2020-08-08 12:42:47 -0500

    20200808

---
+++ Algorithms_medium/0437. Path Sum III.py
@@ -0,0 +1,42 @@
+"""
+0437. Path Sum III
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int):
+        def dfs(root, sumlist):
+            if root is None: return 0
+            sumlist = [num + root.val for num in sumlist]
+            return sumlist.count(sum) + dfs(root.left, sumlist) + dfs(root.right, sumlist)
+        
+    return df(root, [])
