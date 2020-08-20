bf30a76613a7219be1318fac4af25b82f98f33ab
vwang0 <bwang@go.wustl.edu>
2020-08-20 14:08:24 -0500

    20200820

--- Concurrency/1114. Print in Order.py
+++ Concurrency/1114. Print in Order.py
@@ -1,3 +1,27 @@
+"""
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+Example 1:
+
+Input: [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+Example 2:
+
+Input: [1,3,2]
+Output: "firstsecondthird"
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+ 
+Note:
+We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seems to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+"""
 import threading
 class Foo:
     def __init__(self):
@@ -5,13 +29,13 @@ class Foo:
         self.__has_first = False
         self.__has_secon = False
 
-    def first(self, printFirst): 
+    def first(self, printFirst):
         with self.__cv:
             printFirst()
             self.__has_first = True
             self.__cv.notifyAll()
 
-    def second(self, printSecond): 
+    def second(self, printSecond):
         with self.__cv:
             while not self.__has_first:
                 self.__cv.wait()
@@ -22,7 +46,8 @@ class Foo:
     def third(self, printThird):
         with self.__cv:
             while not self.__has_second:
-                self.__cv.wait() 
+                self.__cv.wait()
             printThird()
             self.__cv.notifyAll()
+
 
