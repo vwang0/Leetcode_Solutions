d7eab6f31c0881179881d00f2415b235be905e4c
vwang0 <bwang@go.wustl.edu>
2020-08-24 02:18:10 -0500

    20200824

--- misc/find_number with same digits.py
+++ misc/find_number with same digits.py
@@ -9,6 +9,17 @@ def findNum(N):
             return i
     return print('Not Found!')
 
+######### 
+from collections import Counter
+def findNum():
+    i = 1
+    while i:
+        if Counter(str(i)) == Counter(str(2*i)) and Counter(str(2*i)) == Counter(str(3*i)) and Counter(str(3*i)) == Counter(str(4*i)) and Counter(str(4*i)) == Counter(str(5*i)):
+            return i
+        else:
+            i += 1
+
+
 """
 N = 1,000,000
 results：
