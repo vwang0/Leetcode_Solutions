afd4c178e6cc8b682a3d343b63d50e0905016314
vwang0 <bwang@go.wustl.edu>
2020-08-24 01:45:33 -0500

    20200824

--- misc/find_number with same digits.py
+++ misc/find_number with same digits.py
@@ -2,17 +2,12 @@
 Find the smallest number N in a given range num that meet the criteria:
 N, 2N, 3N, 4N, and 5N have the same digits.
 """
-
 from collections import Counter
 def findNum(N):
     for i in range(1,N):
-        if Counter(str(i))== Counter(str(2*i)) 
-        and Counter(str(2*i)) == Counter(str(3*i)) 
-        and Counter(str(3*i)) == Counter(str(4*i)) 
-        and Counter(str(4*i)) == Counter(str(5*i)):
+        if Counter(str(i))== Counter(str(2*i)) and Counter(str(2*i)) == Counter(str(3*i))  and Counter(str(3*i)) == Counter(str(4*i)) and Counter(str(4*i)) == Counter(str(5*i)):
             return i
     return print('Not Found!')
-
 
 """
 N = 1,000,000
