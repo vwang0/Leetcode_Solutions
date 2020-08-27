8a55f5293eb4110a676bf75500ad1e7abcddd4fa
vwang0 <bwang@go.wustl.edu>
2020-08-27 01:03:42 -0500

    20200827

--- Algorithms_easy/0680. Valid Palindrome II.py
+++ Algorithms_easy/0680. Valid Palindrome II.py
@@ -15,7 +15,9 @@ The string will only contain lowercase characters a-z. The maximum length of the
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         i = 0
-        while i < len(s) // 2 and s[i] == s[-(i + 1)]: 
+        while i < len(s) // 2 and s[i] == s[-(i + 1)]:
             i += 1
-        s = s[i:len(s) - i]
-        return s[1:] == s[1:][::-1] or s[:-1] == s[:-1][::-1]        
+        ss = s[i:len(s) - i]
+        return ss[1:] == ss[1:][::-1] or ss[:-1] == ss[:-1][::-1]
+
+
