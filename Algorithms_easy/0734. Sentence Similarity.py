7dbfa6039aa2048681ff42d9279cf0b355444685
vwang0 <bwang@go.wustl.edu>
2020-08-27 23:23:13 -0500

    20200827

---
+++ Algorithms_easy/0734. Sentence Similarity.py
@@ -0,0 +1,36 @@
+"""
+0734. Sentence Similarity
+Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
+
+For example, "great acting skills" and "fine drama talent" are similar, if the similar word pairs are pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]].
+
+Note that the similarity relation is not transitive. For example, if "great" and "fine" are similar, and "fine" and "good" are similar, "great" and "good" are not necessarily similar.
+
+However, similarity is symmetric. For example, "great" and "fine" being similar is the same as "fine" and "great" being similar.
+
+Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
+
+Finally, sentences can only be similar if they have the same number of words. So a sentence like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
+
+Note:
+
+The length of words1 and words2 will not exceed 1000.
+The length of pairs will not exceed 2000.
+The length of each pairs[i] will be 2.
+The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+"""
+from collections import defaultdict
+
+class Solution:
+    def areSentencesSimilar(self, words1: List[str], words2: List[str], pairs: List[List[str]]) -> bool:
+        if len(words1) != len(words2):
+            return False
+        words = defaultdict(set)
+        for w1, w2 in pairs:
+            words[w1].add(w2)
+            words[w2].add(w1)
+        for wd1, wd2 in zip(words1, words2):
+            if wd1 != wd2 and wd2 not in words[wd1]:
+                return False
+        return True
+
