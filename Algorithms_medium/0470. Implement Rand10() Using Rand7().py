3a763db0df1522435080e2524aa6165a387f4115
vwang0 <bwang@go.wustl.edu>
2020-08-28 10:16:36 -0500

    20200828

---
+++ Algorithms_medium/0470. Implement Rand10() Using Rand7().py
@@ -0,0 +1,50 @@
+"""
+0470. Implement Rand10() Using Rand7()
+Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10.
+
+Do NOT use system's Math.random().
+
+Example 1:
+
+Input: 1
+Output: [7]
+Example 2:
+
+Input: 2
+Output: [8,4]
+Example 3:
+
+Input: 3
+Output: [8,1,10]
+ 
+
+Note:
+
+rand7 is predefined.
+Each testcase has one argument: n, the number of times that rand10 is called.
+ 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+https://leetcode-cn.com/problems/implement-rand10-using-rand7/solution/cong-zui-ji-chu-de-jiang-qi-ru-he-zuo-dao-jun-yun-/
+
+"""
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        rand40 = 40
+        while rand40 >= 40:
+            rand40 = (rand7() - 1) * 7 + rand7() - 1
+        return rand40 % 10 + 1
+
+
+
+
