7d95a4184dfdb7e514735a2d09b1f91dc73cc045
vwang0 <bwang@go.wustl.edu>
2020-08-30 10:36:40 -0500

    20200830

--- Algorithms_hard/0952. Largest Component Size by Common Factor.py
+++ Algorithms_hard/0952. Largest Component Size by Common Factor.py
@@ -29,6 +29,8 @@ Note:
 1 <= A[i] <= 100000
 
 """
+
+
 class Solution:
     def largestComponentSize(self, A: List[int]) -> int:
         primes = []
@@ -39,7 +41,7 @@ class Solution:
             else:
                 primes.append(x)
 
-        factors = collections.defaultdict(list)  # compute factors of each 'a'
+        factors = collections.defaultdict(list)
         for a in A:
             x = a
             for p in primes:
@@ -49,15 +51,15 @@ class Solution:
                     factors[a].append(p)
                     while x % p == 0:
                         x //= p
-            if x > 1:  # a new prime found
+            if x > 1:
                 factors[a].append(x)
                 primes.append(x)
 
         primes = list(set(primes))
         n = len(primes)
-        p2i = {p: i for i, p in enumerate(primes)}  # prime to index
+        p2i = {p: i for i, p in enumerate(primes)}
 
-        parent = [i for i in range(n)]  # union-find on primes
+        parent = [i for i in range(n)]
 
         def find(i):
             if i != parent[i]:
@@ -72,13 +74,11 @@ class Solution:
         for a in A:
             if factors[a]:
                 p0 = factors[a][0]
-                for p in factors[a][
-                        1:]:  # link two primes if they are factors of 'a'
+                for p in factors[a][1:]:
                     union(p2i[p0], p2i[p])
 
         count = collections.Counter(
-            find(p2i[factors[a][0]]) for a in A
-            if factors[a])  # each 'a' corresponds to a prime index
+            find(p2i[factors[a][0]]) for a in A if factors[a])
         return max(count.values())
 
 
