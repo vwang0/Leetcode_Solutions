8869c85f14bf33da1da4f9a9defb3327b49c2040
vwang0 <bwang@go.wustl.edu>
2020-08-30 00:23:54 -0500

    20200830

--- Contest/204/1567. Maximum Length of Subarray With Positive Product.py
+++ Contest/204/1567. Maximum Length of Subarray With Positive Product.py
@@ -44,7 +44,7 @@ class Solution:
         pos, neg = [0] * n, [0] * n
         if nums[0] > 0: pos[0] = 1
         if nums[0] < 0: neg[0] = 1
-        ans = pos[0]
+        res = pos[0]
         for i in range(1, n):
             if nums[i] > 0:
                 pos[i] = 1 + pos[i - 1]
@@ -52,6 +52,5 @@ class Solution:
             elif nums[i] < 0:
                 pos[i] = 1 + neg[i - 1] if neg[i - 1] > 0 else 0
                 neg[i] = 1 + pos[i - 1]
-            ans = max(ans, pos[i])
-        return ans
-
+            res = max(res, pos[i])
+        return res
