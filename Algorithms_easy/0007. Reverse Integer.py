23476ae666263d8da16861a5e02b1930592709e9
vwang0 <bwang@go.wustl.edu>
2020-09-07 02:04:51 -0500

    20200906

--- Algorithms_easy/0007. Reverse Integer.py
+++ Algorithms_easy/0007. Reverse Integer.py
@@ -11,27 +11,36 @@ Example 3:
 Input: 120
 Output: 21
 """
-class Solution:
-    def reverse(self, x: int) -> int:
-        sign = (x>0) - (x<0)
-        res = int(str(sign*x)[::-1])
-        y = (res< (1<<31)) * res * sign
-        return y
-
-a = Solution()
-a.reverse(123)
-a.reverse(-123)
-a.reverse(120)
 
 class Solution:
     def reverse(self, x: int) -> int:
         if x>-2**31  and x < 2**31-1 :
             sign = (x>0) - (x<0)
             x_str = str(sign*x)
-            res = sign*int(''.join(i for i in x_str[::-1])) 
+            res = sign*int(''.join(i for i in x_str[::-1]))
             if res>-2**31  and res < 2**31-1 :
                 return res
-            else: 
+            else:
                 return 0
         else:
             return 0
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        if x > -2**31 and x < 2**31 - 1:
+            sign = -1 if x < 0 else 1
+            x_abs = x * sign
+            while x_abs > 0:
+                x_abs, mod = x_abs // 10, x_abs % 10
+                res = res * 10 + mod
+            if res > -2**31 and res < 2**31 - 1:
+                return sign * res
+            else:
+                return 0
+        else:
+            return 0
+
+
+            
