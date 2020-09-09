ae9f1ffa6e9291321e3b505af8dfdb3c6babb97e
vwang0 <bwang@go.wustl.edu>
2020-09-09 01:48:30 -0500

    20200909

---
+++ Algorithms_easy/0897. Increasing Order Search Tree.py
@@ -0,0 +1,77 @@
+"""
+0897. Increasing Order Search Tree
+Easy
+
+Given a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
+
+Example 1:
+Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+       5
+      / \
+    3    6
+   / \    \
+  2   4    8
+ /        / \ 
+1        7   9
+
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+            \
+             7
+              \
+               8
+                \
+                 9  
+ 
+
+Constraints:
+
+The number of nodes in the given tree will be between 1 and 100.
+Each node will have a unique integer value from 0 to 1000.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        new = head = TreeNode(0)
+        stack = []
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            new.right = root
+            new = new.right
+            root = root.right
+            new.left = None
+
+        return head.right
+
+
+class Solution:
+    def increasingBST(self, root, tail=None):
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
+
+
+        
+
