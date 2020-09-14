9b8891f905a3206cf27a9f98f52f91e5388f8b95
vwang0 <bwang@go.wustl.edu>
2020-09-14 11:40:47 -0500

    20200914

--- Algorithms_easy/0198. House Robber.py
+++ Algorithms_easy/0198. House Robber.py
@@ -17,13 +17,25 @@ Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
 """
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:: int
         total, totalMax = 0, 0
         for num in nums:
             totalMax, total = max(total + num, totalMax), totalMax
         return totalMax
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: return 0
+        size = len(nums)
+        if size == 1: return nums[0]
+
+        dp = [0] * size
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, size):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        return dp[size-1]
+
+
+        
