910b7ee8048d16a2d25d6e022259526bb5206a28
vwang0 <bwang@go.wustl.edu>
2020-09-23 16:59:45 -0500

    20200923

--- SQL/1549. The Most Recent Orders for Each Product.sql
+++ SQL/1549. The Most Recent Orders for Each Product.sql
@@ -110,3 +110,19 @@ ORDER BY product_name, product_id, order_id
 
 
 
+----------- Solution 2
+select b.product_name,
+       a.product_id,
+       a.order_id,
+       a.order_date
+from Orders a
+join Products b on a.product_id = b.product_id
+where (a.product_id,
+       a.order_date) in
+        (select product_id,
+                max(order_date) as order_date
+         from Orders
+         group by product_id)
+order by b.product_name,
+         a.product_id,
+         a.order_id
