01d5a4e3ef46304f2a75c0a2896ada9fe02db505
vwang0 <bwang@go.wustl.edu>
2020-09-27 00:26:42 -0500

    20200927

--- Algorithms_easy/1037. Valid Boomerang.py
+++ Algorithms_easy/1037. Valid Boomerang.py
@@ -27,6 +27,3 @@ class Solution:
         return (points[0][0] - points[1][0]) * (
             points[0][1] - points[2][1]) != (points[0][0] - points[2][0]) * (
                 points[0][1] - points[1][1])
-
-
-
