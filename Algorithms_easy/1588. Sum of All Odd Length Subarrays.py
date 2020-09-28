d4efc032a9245cac324ca82b0835304bdde0d7b3
vwang0 <bwang@go.wustl.edu>
2020-09-27 20:04:21 -0500

    20200927

--- Algorithms_easy/1588. Sum of All Odd Length Subarrays.py
+++ Algorithms_easy/1588. Sum of All Odd Length Subarrays.py
@@ -46,3 +46,13 @@ class Solution:
             for j in range(n - i + 1):
                 res += sum(arr[j: j + i])
         return res
+
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        res, n = 0, len(arr)
+        for i, a in enumerate(arr):
+            res += ((i + 1) * (n - i) + 1) // 2 * a
+        return res
+
+
