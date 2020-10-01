0e171256fcc8065ef2a5d9df58964905e8a7224a
vwang0 <bwang@go.wustl.edu>
2020-10-01 08:46:17 -0500

    20201001

---
+++ Algorithms_easy/1189. Maximum Number of Balloons.py
@@ -0,0 +1,34 @@
+"""
+1189. Maximum Number of Balloons
+Easy
+
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+
+Input: text = "leetcode"
+Output: 0
+ 
+Constraints:
+
+1 <= text.length <= 10^4
+text consists of lower case English letters only.
+"""
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        cnt_text = collections.Counter(text.lower())
+        cnt_balloon = collections.Counter('balloon')
+        return min(cnt_text[c]//cnt_balloon[c] for c in cnt_balloon)
+
+
+        
