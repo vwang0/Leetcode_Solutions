cab1048d714158d29586022e6ef9cb32d31067db
Vic <bwang@go.wustl.edu>
2020-10-11 02:47:56 -0500

    20201011

---
+++ Algorithms_medium/1081. Smallest Subsequence of Distinct Characters.py
@@ -0,0 +1,40 @@
+"""
+1081. Smallest Subsequence of Distinct Characters
+Medium
+
+same as  316
+
+Return the lexicographically smallest subsequence of s that contains all the distinct characters of s exactly once.
+
+Note: This question is the same as 316: https://leetcode.com/problems/remove-duplicate-letters/
+
+ 
+
+Example 1:
+
+Input: s = "bcabc"
+Output: "abc"
+Example 2:
+
+Input: s = "cbacdcbc"
+Output: "acdb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+"""
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        stack = []
+        remain_counter = collections.Counter(s)
+
+        for c in s:
+            if c not in stack:
+                while stack and c < stack[-1] and  remain_counter[stack[-1]] > 0:
+                    stack.pop()
+                stack.append(c)
+            remain_counter[c] -= 1
+        return ''.join(stack)
