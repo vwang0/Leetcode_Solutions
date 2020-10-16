b24dc02c696aba30a902fb73439cc5364bd6213a
Vic <bwang@go.wustl.edu>
2020-10-16 02:46:46 -0500

    20201016

---
+++ Algorithms_medium/0074. Search a 2D Matrix.py
@@ -0,0 +1,52 @@
+"""
+0074. Search a 2D Matrix
+Medium
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+Output: false
+Example 3:
+
+Input: matrix = [], target = 0
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+0 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+"""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+                pivot_idx = (left + right) // 2
+                pivot_element = matrix[pivot_idx // n][pivot_idx % n]
+                if target == pivot_element:
+                    return True
+                else:
+                    if target < pivot_element:
+                        right = pivot_idx - 1
+                    else:
+                        left = pivot_idx + 1
+        return False
+        
