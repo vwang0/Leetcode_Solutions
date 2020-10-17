eda5b4878a3c5f67f799888c52e5e245e4c8e5ed
vwang0 <bwang@go.wustl.edu>
2020-10-16 22:57:19 -0500

    20201016

--- Algorithms_medium/0739. Daily Temperatures.py
+++ Algorithms_medium/0739. Daily Temperatures.py
@@ -10,4 +10,11 @@ Note: The length of temperatures will be in the range [1, 30000]. Each temperatu
 """
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
-        
+        res = [0] * len(T)
+        stack = []
+        for i, t in enumerate(T):
+            while stack and T[stack[-1]] < t:
+                cur = stack.pop()
+                res[cur] = i - cur
+            stack.append(i)
+        return res
