be2227951927eb2a709f4b36e62ab125f59d67f4
vwang0 <bwang@go.wustl.edu>
2020-11-07 02:56:57 -0600

    20201107

--- Algorithms_medium/0002. Add Two Numbers.py
+++ Algorithms_medium/0002. Add Two Numbers.py
@@ -15,20 +15,36 @@ Explanation: 342 + 465 = 807.
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
-        root = n = ListNode(0)
+        root = temp = ListNode(0)
         while l1 or l2 or carry:
-            v1 = v2 = 0
-            if l1:
+            v1 = v2 = 0 
+            if l1: 
                 v1 = l1.val
                 l1 = l1.next
             if l2:
                 v2 = l2.val
                 l2 = l2.next
-            carry, val = divmod(v1+v2+carry, 10)
-            n.next = ListNode(val)
-            n = n.next
+            carry, val = divmod(v1 + v2 + carry, 10)
+            temp.next = ListNode(val)
+            temp = temp.next
         return root.next        
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next
