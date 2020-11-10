8794e8431886427d598b8ece16c99e510faa03e6
Vic <bwang@go.wustl.edu>
2020-11-10 11:25:15 -0500

    20201109

--- Algorithms_easy/1099. Two Sum Less Than K.py
+++ Algorithms_easy/1099. Two Sum Less Than K.py
@@ -8,13 +8,13 @@ Example 1:
 
 Input: A = [34,23,1,24,75,33,54,8], K = 60
 Output: 58
-Explanation: 
+Explanation:
 We can use 34 and 24 to sum 58 which is less than 60.
 Example 2:
 
 Input: A = [10,20,30], K = 15
 Output: -1
-Explanation: 
+Explanation:
 In this case it's not possible to get a pair sum less that 15.
 
 Note:
@@ -32,6 +32,16 @@ class Solution:
                 if temp < K:
                     sum2 = max(sum2, temp)
         return -1 if sum2 == float('-inf') else sum2
+
+class Solution:
+    def twoSumLessThanK(self, A: List[int], K: int) -> int:
+        sum2 = float('-inf')
+        for i in range(len(A)):
+            for j in range(i, len(A)):
+                temp = A[i] + A[j]
+                if temp < K:
+                    sum2 = max(sum2, temp)
+        return sum2 if sum2 != float('-inf') else -1    
 
 
 class Solution:
