f1080fd05ce7953d63eadedbdb9b5d1367c85817
vwang0 <bwang@go.wustl.edu>
2020-11-18 08:51:26 -0600

    20201118

--- Algorithms_medium/0056. Merge Intervals.py
+++ Algorithms_medium/0056. Merge Intervals.py
@@ -31,3 +31,18 @@ class Solution(object):
             else:
                 res[-1][1] = max(res[-1][1], elem[1])
         return res
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        intervals = sorted(intervals, key = lambda x: x[0])
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] > res[-1][1]:
+                res.append(intervals[i])
+            else:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+        return res
+               
