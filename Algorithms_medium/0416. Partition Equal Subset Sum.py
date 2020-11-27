b6c0d53a4d66fd86e2209d87d4241caba5fe9922
vwang0 <bwang@go.wustl.edu>
2020-11-27 05:52:51 -0600

    20201127

---
+++ Algorithms_medium/0416. Partition Equal Subset Sum.py
@@ -0,0 +1,30 @@
+"""
+0416. Partition Equal Subset Sum
+Medium
+
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+"""
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        possible_sums = {0}
+        for n in nums:
+            possible_sums.update({(v + n) for v in possible_sums})
+        return (sum(nums) / 2.)  in possible_sums  
