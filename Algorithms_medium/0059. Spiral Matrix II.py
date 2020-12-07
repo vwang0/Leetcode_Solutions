7f6c700159c94bb689cbfa18aa7ccd5fcab4acd1
vwang0 <bwang@go.wustl.edu>
2020-12-07 06:18:19 -0600

    20201107

---
+++ Algorithms_medium/0059. Spiral Matrix II.py
@@ -0,0 +1,32 @@
+"""
+0059. Spiral Matrix II
+Medium
+
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+Example 1:
+
+
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+Example 2:
+
+Input: n = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= n <= 20
+"""
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        A = [[0] * n for _ in range(n)]
+        i, j, di, dj = 0, 0, 0, 1
+        for k in range(n*n):
+            A[i][j] = k + 1
+            if A[(i+di)%n][(j+dj)%n]:
+                di, dj = dj, -di
+            i += di
+            j += dj
+        return A
