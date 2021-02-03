dca8147b5d65daff20caedb36faea5aa5ffd09cb
vwang0 <bwang@go.wustl.edu>
2021-02-03 08:45:17 -0600

    20210203

--- Algorithms_easy/0141. Linked List Cycle.py
+++ Algorithms_easy/0141. Linked List Cycle.py
@@ -47,3 +47,21 @@ class Solution(object):
             if slow == fast:
                 return 1
         return 0
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False        
