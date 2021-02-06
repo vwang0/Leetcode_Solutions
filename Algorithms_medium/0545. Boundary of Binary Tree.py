88c603cc17b6572bbe92118cf288c0ee94864e6e
vwang0 <bwang@go.wustl.edu>
2021-02-06 15:50:29 -0600

    20210206

---
+++ Algorithms_medium/0545. Boundary of Binary Tree.py
@@ -0,0 +1,140 @@
+"""
+0545. Boundary of Binary Tree
+Medium
+
+The boundary of a binary tree is the concatenation of the root, the left boundary, the leaves ordered from left-to-right, and the reverse order of the right boundary.
+
+The left boundary is the set of nodes defined by the following:
+
+The root node's left child is in the left boundary. If the root does not have a left child, then the left boundary is empty.
+If a node in the left boundary and has a left child, then the left child is in the left boundary.
+If a node is in the left boundary, has no left child, but has a right child, then the right child is in the left boundary.
+The leftmost leaf is not in the left boundary.
+The right boundary is similar to the left boundary, except it is the right side of the root's right subtree. Again, the leaf is not part of the right boundary, and the right boundary is empty if the root does not have a right child.
+
+The leaves are nodes that do not have any children. For this problem, the root is not a leaf.
+
+Given the root of a binary tree, return the values of its boundary.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3,4]
+Output: [1,3,4,2]
+Explanation:
+- The left boundary is empty because the root does not have a left child.
+- The right boundary follows the path starting from the root's right child 2 -> 4.
+  4 is a leaf, so the right boundary is [2].
+- The leaves from left to right are [3,4].
+Concatenating everything results in [1] + [] + [3,4] + [2] = [1,3,4,2].
+Example 2:
+
+
+Input: root = [1,2,3,4,5,6,null,null,null,7,8,9,10]
+Output: [1,2,4,7,8,9,10,6,3]
+Explanation:
+- The left boundary follows the path starting from the root's left child 2 -> 4.
+  4 is a leaf, so the left boundary is [2].
+- The right boundary follows the path starting from the root's right child 3 -> 6 -> 10.
+  10 is a leaf, so the right boundary is [3,6], and in reverse order is [6,3].
+- The leaves from left to right are [4,7,8,9,10].
+Concatenating everything results in [1] + [2] + [4,7,8,9,10] + [6,3] = [1,2,4,7,8,9,10,6,3].
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-1000 <= Node.val <= 1000
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: TreeNode) -> List[int]:
+        def dfs_leftmost(node):
+            if not node or not node.left and not node.right:
+                return
+            boundary.append(node.val)
+            if node.left:
+                dfs_leftmost(node.left)
+            else:
+                dfs_leftmost(node.right)
+
+        def dfs_leaves(node):
+            if not node:
+                return
+            dfs_leaves(node.left)
+            if node != root and not node.left and not node.right:
+                boundary.append(node.val)
+            dfs_leaves(node.right)
+
+        def dfs_rightmost(node):
+            if not node or not node.left and not node.right:
+                return
+            if node.right:
+                dfs_rightmost(node.right)
+            else:
+                dfs_rightmost(node.left)
+            boundary.append(node.val)
+
+        if not root: return []
+        boundary = [root.val]
+        dfs_leftmost(root.left)
+        dfs_leaves(root)
+        dfs_rightmost(root.right)
+        return boundary
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        left_bd_nodes = [root]
+        cur = root.left
+        while cur:
+            left_bd_nodes.append(cur)
+            cur = cur.left or cur.right
+
+        right_bd_nodes = [root]
+        cur = root.right
+        while cur:           
+            right_bd_nodes.append(cur)
+            cur = cur.right or cur.left
+
+        leaf_nodes = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            if not node.left and not node.right:
+                leaf_nodes.append(node)
+
+        res = []
+        seen = set()
+
+        def visit(node):
+            if node not in seen:
+                seen.add(node)
+                res.append(node.val)
+
+        for node in left_bd_nodes: 
+            visit(node)
+        for node in leaf_nodes: 
+            visit(node)
+        for node in reversed(right_bd_nodes): 
+            visit(node)
+
+        return res
