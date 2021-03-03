fb347ffa375ce71fcc64ca0b953e38dd87108d2e
vwang0 <bwang@go.wustl.edu>
2021-03-03 09:40:01 -0600

    20210303

---
+++ SQL/1767. Find the Subtasks That Did Not Execute.sql
@@ -0,0 +1,85 @@
+"""
+1767. Find the Subtasks That Did Not Execute
+Hard
+
+SQL Schema
+Table: Tasks
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| task_id        | int     |
+| subtasks_count | int     |
++----------------+---------+
+task_id is the primary key for this table.
+Each row in this table indicates that task_id was divided into subtasks_count subtasks labelled from 1 to subtasks_count.
+It is guaranteed that 2 <= subtasks_count <= 20.
+ 
+
+Table: Executed
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| task_id       | int     |
+| subtask_id    | int     |
++---------------+---------+
+(task_id, subtask_id) is the primary key for this table.
+Each row in this table indicates that for the task task_id, the subtask with ID subtask_id was executed successfully.
+It is guaranteed that subtask_id <= subtasks_count for each task_id.
+ 
+
+Write an SQL query to report the IDs of the missing subtasks for each task_id.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+ 
+
+Tasks table:
++---------+----------------+
+| task_id | subtasks_count |
++---------+----------------+
+| 1       | 3              |
+| 2       | 2              |
+| 3       | 4              |
++---------+----------------+
+
+Executed table:
++---------+------------+
+| task_id | subtask_id |
++---------+------------+
+| 1       | 2          |
+| 3       | 1          |
+| 3       | 2          |
+| 3       | 3          |
+| 3       | 4          |
++---------+------------+
+
+Result table:
++---------+------------+
+| task_id | subtask_id |
++---------+------------+
+| 1       | 1          |
+| 1       | 3          |
+| 2       | 1          |
+| 2       | 2          |
++---------+------------+
+Task 1 was divided into 3 subtasks (1, 2, 3). Only subtask 2 was executed successfully, so we include (1, 1) and (1, 3) in the answer.
+Task 2 was divided into 2 subtasks (1, 2). No subtask was executed successfully, so we include (2, 1) and (2, 2) in the answer.
+Task 3 was divided into 4 subtasks (1, 2, 3, 4). All of the subtasks were executed successfully.
+"""
+
+WITH RECURSIVE id AS (
+    SELECT task_id, subtasks_count, 1 AS subtask_id 
+    FROM Tasks
+    UNION ALL 
+    SELECT task_id, subtasks_count, subtask_id + 1 AS subtask_id 
+    FROM id WHERE subtask_id < (SELECT MAX(subtasks_count) FROM Tasks))
+
+SELECT id.task_id, id.subtask_id 
+FROM id 
+LEFT JOIN Executed e ON id.task_id = e.task_id AND id.subtask_id = e.subtask_id 
+WHERE e.subtask_id IS NULL 
+AND id.subtasks_count >= id.subtask_id 
