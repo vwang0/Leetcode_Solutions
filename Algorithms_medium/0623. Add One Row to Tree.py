7ee336ff7639711b06194dac503e3392d0e3eb55
vwang0 <bwang@go.wustl.edu>
2021-03-09 11:20:40 -0600

    20210309

---
+++ Algorithms_medium/0623. Add One Row to Tree.py
@@ -0,0 +1,91 @@
+"""
+0623. Add One Row to Tree
+Medium
+
+Given the root of a binary tree, then value v and depth d, you need to add a row of nodes with value v at the given depth d. The root node is at depth 1.
+
+The adding rule is: given a positive integer depth d, for each NOT null tree nodes N in depth d-1, create two tree nodes with value v as N's left subtree root and right subtree root. And N's original left subtree should be the left subtree of the new left subtree root, its original right subtree should be the right subtree of the new right subtree root. If depth d is 1 that means there is no depth d-1 at all, then create a tree node with value v as the new root of the whole original tree, and the original tree is the new root's left subtree.
+
+Example 1:
+Input: 
+A binary tree as following:
+       4
+     /   \
+    2     6
+   / \   / 
+  3   1 5   
+
+v = 1
+
+d = 2
+
+Output: 
+       4
+      / \
+     1   1
+    /     \
+   2       6
+  / \     / 
+ 3   1   5   
+
+Example 2:
+Input: 
+A binary tree as following:
+      4
+     /   
+    2    
+   / \   
+  3   1    
+
+v = 1
+
+d = 3
+
+Output: 
+      4
+     /   
+    2
+   / \    
+  1   1
+ /     \  
+3       1
+Note:
+The given d is in range [1, maximum depth of the given tree + 1].
+The given binary tree has at least one tree node.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        dummy, dummy.left = TreeNode(None), root
+        row = [dummy]
+        for _ in range(d - 1):
+            row = [kid for node in row for kid in (node.left, node.right) if kid]
+        for node in row:
+            node.left, node.left.left = TreeNode(v), node.left
+            node.right, node.right.right = TreeNode(v), node.right
+        return dummy.left
+
+
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        def dfs(node, h, dr):            
+            if h == d:
+                tmp = TreeNode(v)
+                if dr == 0:
+                    tmp.left = node
+                else:
+                    tmp.right = node
+                return tmp
+            
+            if not node: return node
+            
+            node.left = dfs(node.left, h+1, 0)
+            node.right = dfs(node.right, h+1, 1)
+            return node
+            
+        return dfs(root, 1, 0)
