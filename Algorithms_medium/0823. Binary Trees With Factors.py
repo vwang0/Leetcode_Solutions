a3050be15a7729ec205ab40adf465da8ad0628bd
vwang0 <bwang@go.wustl.edu>
2021-03-13 02:17:16 -0600

    20210313

--- Algorithms_medium/0823. Binary Trees With Factors.py
+++ Algorithms_medium/0823. Binary Trees With Factors.py
@@ -31,5 +31,5 @@ class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         dp = {}
         for a in sorted(arr):
-            dp[a] = sum(dp[b]* dp.get(a // b, 0) for b in dp if a % b == 0) + 1
-        return sum(dp.values()) % (10**9 + 7)            
+            dp[a] = sum(dp[b]* dp.get(a / b, 0) for b in dp if a % b == 0) + 1
+        return sum(dp.values()) % (10**9 + 7)      
