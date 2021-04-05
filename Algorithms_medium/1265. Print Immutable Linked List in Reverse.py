c19682fda1bf88119281dd73da6bdfedffe0cac8
vwang0 <bwang@go.wustl.edu>
2021-04-04 21:59:28 -0500

    20210404

---
+++ Algorithms_medium/1265. Print Immutable Linked List in Reverse.py
@@ -0,0 +1,78 @@
+"""
+1265. Print Immutable Linked List in Reverse
+Medium
+
+You are given an immutable linked list, print out all values of each node in reverse with the help of the following interface:
+
+ImmutableListNode: An interface of immutable linked list, you are given the head of the list.
+You need to use the following functions to access the linked list (you can't access the ImmutableListNode directly):
+
+ImmutableListNode.printValue(): Print value of the current node.
+ImmutableListNode.getNext(): Return the next node.
+The input is only given to initialize the linked list internally. You must solve this problem without modifying the linked list. In other words, you must operate the linked list using only the mentioned APIs.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4]
+Output: [4,3,2,1]
+Example 2:
+
+Input: head = [0,-4,-1,3,-5]
+Output: [-5,3,-1,-4,0]
+Example 3:
+
+Input: head = [-2,0,6,4,4,-6]
+Output: [-6,4,4,6,0,-2]
+ 
+
+Constraints:
+
+The length of the linked list is between [1, 1000].
+The value of each node in the linked list is between [-1000, 1000].
+ 
+
+Follow up:
+
+Could you solve this problem in:
+
+Constant space complexity?
+Linear time complexity and less than linear space complexity?
+"""
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        if head:
+            self.printLinkedListInReverse(head.getNext());
+            head.printValue();
+
+
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        cur = head
+        n=0
+        while cur:
+            n+=1
+            cur=cur.getNext()
+        # n is size of LinkedList
+        for i in range(n-1,0-1,-1):
+            cur = head
+            for _ in range(i):
+                cur=cur.getNext()
+            cur.printValue()
