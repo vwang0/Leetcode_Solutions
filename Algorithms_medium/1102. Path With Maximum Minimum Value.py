6eac518dc51011985f37d7deb8c7149bdc93088b
vwang0 <bwang@go.wustl.edu>
2021-04-08 07:59:43 -0500

    20210408

---
+++ Algorithms_medium/1102. Path With Maximum Minimum Value.py
@@ -0,0 +1,56 @@
+"""
+1102. Path With Maximum Minimum Value
+Medium
+
+Given a matrix of integers A with R rows and C columns, find the maximum score of a path starting at [0,0] and ending at [R-1,C-1].
+
+The score of a path is the minimum value in that path.  For example, the value of the path 8 →  4 →  5 →  9 is 4.
+
+A path moves some number of times from one visited cell to any neighbouring unvisited cell in one of the 4 cardinal directions (north, east, west, south).
+
+ 
+
+Example 1:
+
+
+
+Input: [[5,4,5],[1,2,6],[7,4,6]]
+Output: 4
+Explanation: 
+The path with the maximum score is highlighted in yellow. 
+Example 2:
+
+
+
+Input: [[2,2,1,2,2,2],[1,2,2,2,1,2]]
+Output: 2
+Example 3:
+
+
+
+Input: [[3,4,6,3,4],[0,2,1,1,7],[8,8,3,2,7],[3,2,4,9,8],[4,1,2,0,0],[4,6,5,4,3]]
+Output: 3
+ 
+
+Note:
+
+1 <= R, C <= 100
+0 <= A[i][j] <= 10^9
+"""
+class Solution:
+    def maximumMinimumPath(self, A: List[List[int]]) -> int:
+        dire = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        R, C = len(A), len(A[0])
+        
+        maxHeap = [(-A[0][0], 0, 0)]
+        seen = [[0 for _ in range(C)] for _ in range(R)]
+        while maxHeap:
+            val, x, y = heapq.heappop(maxHeap)
+            # seen[x][y] = 1 # got TLE
+            if x == R - 1 and y == C - 1: return -val
+            for dx, dy in dire:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < R and 0 <= ny < C and not seen[nx][ny]:
+                    seen[nx][ny] = 1 # passed
+                    heapq.heappush(maxHeap, (max(val, -A[nx][ny]), nx, ny))
+        return -1
